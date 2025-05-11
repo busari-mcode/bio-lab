a42bb53d8056f81dace4290aa50e471935e57aa8
Busari Moruf <busarimoruf20@gmail.com>
2025-05-10 23:41:23 -0700

    new changes

--- src/components/people/People.jsx
+++ src/components/people/People.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import { team } from '../../dummydata'
+// import images from '../../../public'
+// import test_images from '../../test_images'
+import prof from '../../test_images/Vasdekis_Andreas_UIdaho_photob.avif'
+
+// import 'public\images\Vasdekis_Andreas_UIdaho_photob.avif'
+// import { team } from 'public\images\Vasdekis_Andreas_UIdaho_photob.avif'
 
 const People = () => {
   return (
     <>
         <section className="team">
-            {team.map((val) => {
+            {/* {team.map((val) => {
               <div className="img"> 
                 <img src={val.cover} alt=""/>
               </div>
-            })}
+            })} */}
+            <h1>He</h1>
+            <img src={prof} alt="" />
         </section>
     </>
   )
 }
-
 export default People
