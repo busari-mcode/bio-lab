8c698adb44f0840c0c8537d66346e374ac86f277
Busari Moruf <busarimoruf20@gmail.com>
2025-05-19 09:33:15 -0700

    new changes

--- src/components/publications/Publications.jsx
+++ src/components/publications/Publications.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import './publications.css'
 
 const Publications = () => {
   return (
     <>
         <section className="publications">
-            <h1>publications</h1>
+            <div className="top_section">
+              <div className="book">Book Chapter</div>
+              <div className="journals">Journals</div>
+              <div className="patent">Patent</div>
+            </div>
+
         </section>
     </>
   )
