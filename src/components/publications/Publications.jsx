4a0f98007e5ef6382a788f3646399c5d05fac8e1
Busari Moruf <busarimoruf20@gmail.com>
2025-05-20 23:55:02 -0700

    new changes

--- src/components/publications/Publications.jsx
+++ src/components/publications/Publications.jsx
@@ -11,7 +11,7 @@ const Publications = () => {
               <div className="patent">Patents</div>
             </div>
             <div className="book_chp">
-              <div>Book Chapters</div>
+              <h6>Book Chapters</h6>
               <ol>
                 <li>A. E. Vasdekis, G. Stephanopoulos, <a href="https://link.springer.com/chapter/10.1007/978-3-319-30019-1_9" target="_blank">'Single Cell Phenotypic Screening in Inverse Metabolic Engineering', in Microfluidic Methods for Molecular Biology</a> Springer (2016).</li>
               </ol>
