bed34c807d0dfba91826e31da7b9301a527946d3
Busari Moruf <busarimoruf20@gmail.com>
2025-05-28 23:48:28 -0700

    new changes

--- src/components/join/Join.jsx
+++ src/components/join/Join.jsx
@@ -11,14 +11,14 @@ const Join = () => {
               <div className="phd_pos">
                 <h4>Join us!</h4>
                 <h5>Ph.D. Positions</h5>
-                <p>We accept Ph.D. student applications from the <a href="">Physics, Bioinformatics and Computational Biology and Chemical and Biological Engineering</a> graduate programs. Contact <a href="" target="_blank">Andreas</a> for more info.</p>
+                <p>We accept Ph.D. student applications from the <a href="https://www.uidaho.edu/sci/bcb" target='_blank'>Physics, Bioinformatics and Computational Biology and Chemical and Biological Engineering</a> graduate programs. Contact <a href="mailto:andreasv@uidaho.edu" target="_blank">Andreas</a> for more info.</p>
               </div>
               <img src={mountain} alt="" />
             </div>
             <div className="postdoc_pos_sect">
               <div className="postdoc_pos">
                 <h5>Postdoctoral Positions</h5>
-                <p>We currently do not have openings. But if interested in our research, please get in <a href="">touch</a> to discuss further.</p>
+                <p>We currently do not have openings. But if interested in our research, please get in <a href="mailto:andreasv@uidaho.edu" target='_blank'>touch</a> to discuss further.</p>
               </div>
             <img src={river} alt="" />
             </div>
