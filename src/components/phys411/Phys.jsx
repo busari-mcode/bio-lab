0cf2888713b4441fd5e7733522652d59cad3bbbe
Busari Moruf <busarimoruf20@gmail.com>
2025-06-04 14:58:19 -0700

    new changes

--- src/components/phys411/Phys.jsx
+++ src/components/phys411/Phys.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import './phys.css'
 
 const Phys = () => {
   return (
     <>
         <section className="phys">
             <p className="instruction">click on the icon to download the images and lecture notes:</p>
+            <div className="icon">
+              <a href="/files/aevlab_phys411.zip" download><i class="fa-regular fa-eye"></i></a>
+            </div>
         </section>
     </>
   )
