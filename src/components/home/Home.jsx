ce6053b1e5fb2a8f2626a823d326bc076995a4ef
Busari Moruf <busarimoruf20@gmail.com>
2025-06-18 11:21:04 -0700

    new changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './home.css'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import cord from '../../test_images/3-d.avif'
+import microfludics from '../../test_images/microfludics.avif'
+import targetsample from '../../test_images/target-sample.avif'
+import laser from '../../test_images/laser-spot.gif'
+import cells from '../../test_images/live-cells-trapping.gif'
 
 const Home = () => {
   return (
@@ -19,21 +24,21 @@ const Home = () => {
             </ul>
             <div className="images">
                 <div className="cord">
-                    <img src="../../images/3-d.avif" alt="" />
+                    <img src={cord} alt="" />
                 </div>
                 <div className="microfludics">
-                    <img src="../../images/microfludics.avif" alt="" />
+                    <img src={microfludics} alt="" />
                 </div>
                 <div className="set3">
                     <div className="target-sample">
-                        <img src="../../images/target-sample.avif" alt="" />
+                        <img src={targetsample} alt="" />
                     </div>
-                    <div className="imaging">
-                        <img src="../../images/imaging.gif" alt="" />
+                    <div className="laser-spot">
+                        <img src={laser} alt="" /> 
                     </div>
                 </div>
                 <div className="live-cells">
-                    <img src="../../images/live-cells-trapping.gif" alt="" />
+                    <img src={cells} alt="" />
                 </div>
             </div>
         </section>
