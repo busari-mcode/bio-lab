b15d19a77b5c9215c2e88655efe108bcf913d462
Busari Moruf <busarimoruf20@gmail.com>
2025-06-21 04:51:08 -0700

    new changes

--- src/components/heading/Header.jsx
+++ src/components/heading/Header.jsx
@@ -21,6 +21,31 @@ const Header = () => {
                 <i class="fa-solid fa-circle-user"></i>
                 <h4>Log In</h4>
             </div>
+
+            {/************ mobile menu ***********/}            
+            <div className="menu-icon">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+            <div className="mobile-menu">
+                <div className="mobile-auth">
+                    <i class="fa-solid fa-circle-user"></i>
+                    <h4>Log In</h4>
+                </div>
+                <ul>
+                    <li><Link to="/" className='page'>home</Link></li>
+                    <li>
+                        <Link to="/people" className='people'>people</Link>
+                        <i class="fa-solid fa-xmark"></i>
+                    </li>
+                    <li><Link to="/publications" className='publications'>publications</Link></li>
+                    <li><Link to="/labpics" className='labpics'>lab pictures</Link></li>
+                    <li><Link to="/join" className='join'>join</Link></li>
+                    <li><Link to="/animations" className='animations'>animations</Link></li>
+                    <li><Link to="/teaching" className='teaching'>teaching (just the fun stuff)</Link></li>
+                    <li><Link to="/phys" className='phys'>University of Idaho - PHYS411</Link></li>
+                    <li><Link to="/equipbook" className='equipbook'>equipment booking</Link></li>
+                </ul>
+            </div>
         </section>
         ) }
 
